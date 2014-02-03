41a38f2792708859bde07ae25b3a10c2752c131f
Tim Preuß <tim.preuss@appcellent.de>
2014-02-03 11:00:31 +0100

    updated comment in header file

--- ios-linechart/LCLineChartView.h
+++ ios-linechart/LCLineChartView.h
@@ -52,7 +52,7 @@ typedef LCLineChartDataItem *(^LCLineChartDataGetter)(NSUInteger item);
 @property NSUInteger xStepsCount; /// number of steps in x. At each x step, a vertical scale line is shown. if x < 2, nothing is done
 
 @property BOOL smoothPlot; // draw a smoothed Bezier plot? Default: NO
-@property BOOL smoothXAxisOnly; // limit the smoothing to horizontal only. Default YES.
+@property BOOL smoothXAxisOnly; // limit the smoothing to horizontal only. Default NO.
 @property BOOL drawsDataPoints; // Switch to turn off circles on data points. On by default.
 @property BOOL drawsDataLines; // Switch to turn off lines connecting data points. On by default.
 @property BOOL fillPlot; // fill the area under the curve.
